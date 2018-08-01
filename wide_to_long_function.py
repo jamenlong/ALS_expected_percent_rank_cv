ef37e308a897a1da7c005d37c90e0ff232e0f58d
Jamen <jamenlong@yahoo.com>
2018-07-31 18:57:51 -0700

    Create wide_to_long_function.py

---
+++ wide_to_long_function.py
@@ -0,0 +1,15 @@
+# This function was originally provided for use in converting the MovieLens ratings dataframe from a conventional/"wide"
+# dataframe to a row-based/"long" dataframe.
+
+from pyspark.sql.functions import array, col, explode, lit, struct
+
+def to_long(df, by = ["userId"]): # "by" is the column by which you want the final output dataframe to be grouped by
+
+    cols = [c for c in df.columns if c not in by] 
+    
+    kvs = explode(array([struct(lit(c).alias("variable"), col(c).alias("rating")) for c in cols])).alias("kvs") 
+    
+    long_df = df.select(by + [kvs]).select(by + ["kvs.variable", "kvs.rating"]).filter("rating IS NOT NULL")
+    # Excluding null ratings values since ALS in Pyspark doesn't want blank/null values
+             
+    return long_df
