db27b9d1edab52fb5cac75323bdcfee90c2369bf
Jamen <jamenlong@yahoo.com>
2018-07-31 19:18:06 -0700

    Update wide_to_long_function.py

--- wide_to_long_function.py
+++ wide_to_long_function.py
@@ -7,9 +7,9 @@ def to_long(df, by = ["userId"]): # "by" is the column by which you want the fin
 
     cols = [c for c in df.columns if c not in by] 
     
-    kvs = explode(array([struct(lit(c).alias("variable"), col(c).alias("rating")) for c in cols])).alias("kvs") 
+    kvs = explode(array([struct(lit(c).alias("movieId"), col(c).alias("rating")) for c in cols])).alias("kvs") 
     
-    long_df = df.select(by + [kvs]).select(by + ["kvs.variable", "kvs.rating"]).filter("rating IS NOT NULL")
+    long_df = df.select(by + [kvs]).select(by + ["kvs.movieId", "kvs.rating"]).filter("rating IS NOT NULL")
     # Excluding null ratings values since ALS in Pyspark doesn't want blank/null values
              
     return long_df
